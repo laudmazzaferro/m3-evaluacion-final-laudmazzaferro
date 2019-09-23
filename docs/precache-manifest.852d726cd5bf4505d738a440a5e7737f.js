bb175ee69675f048d33b08907648448d0bd00161
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-09-23 22:54:56 +0200

    do npm build docs

---
+++ docs/precache-manifest.852d726cd5bf4505d738a440a5e7737f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5c0cf9f4623ad788c0a040055bacc7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e648039ee8d43944b107",
+    "url": "./static/css/main.d4e7bd86.chunk.css"
+  },
+  {
+    "revision": "7ae4738b0f2a75568499",
+    "url": "./static/js/2.f5636ba6.chunk.js"
+  },
+  {
+    "revision": "e648039ee8d43944b107",
+    "url": "./static/js/main.b529e50e.chunk.js"
+  },
+  {
+    "revision": "815c50b41fb31db34da9",
+    "url": "./static/js/runtime-main.eb25eba7.js"
+  }
+]);
