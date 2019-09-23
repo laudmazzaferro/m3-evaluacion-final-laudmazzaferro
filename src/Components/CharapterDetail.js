689ff1906c674e51b86cb18141488f8f9b90c62e
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-09-23 13:12:17 +0200

    do the card detail

--- src/Components/CharapterDetail.js
+++ src/Components/CharapterDetail.js
@@ -3,27 +3,33 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const CharapterDetail = props => {
-    const { routerProps, charapters } = this.props;
+    const { routerProps, charapters } = props;
     const chapId = parseInt(routerProps.match.params.chapId);
     const charapter = charapters.filter(item => item.id === chapId);
-    console.log(charapter[0])
-
+    
     if (charapter[0]){
-      const {name ,image, status, species, origin, episodes} = charapter[0];
+      const {name ,image, status, species, origin, episode} = charapter[0];
       return (
         <React.Fragment>
             <div>
               <img src={image} alt={name}/>
             </div>
-            <h3>{name}</h3>
+            <h3>{`${name}`}</h3>
             <p>{`Status:${status}`}</p>
-            <p>Species:{species}</p>
-            <p>Origin:{origin}</p>
-            <p>Episodes:{episodes}</p>
+            <p>{`Species:${species}`}</p>
+            <p>{`Origin:${origin.name}`}</p>
+            <p>{`Episodes:${episode.length}`}</p>
           <Link to="/" className="app__back">Volver al listado</Link>
         </React.Fragment>
         
       );
+    }else {
+      return(
+        <React.Fragment>
+        <p>Personaje no existente</p>
+        <Link to="/" className="app__back">Volver al listado</Link>
+        </React.Fragment>
+      )
     }
 }
 
