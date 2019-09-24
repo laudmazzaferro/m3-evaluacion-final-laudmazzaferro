00a48aa9597b73b47492a0150e37bc46582bcebf
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-09-24 20:05:59 +0200

    add radio filter to the web

--- src/Components/Filters/Filters.js
+++ src/Components/Filters/Filters.js
@@ -4,26 +4,62 @@ import './Filters.scss';
 
 class Filters extends React.Component {
   render() {
-    const {userInput,getUserInput,getUserSelect} = this.props
-    return(
+    const { userInput, getUserInput, getUserSelect, getUserRadio, radioValue,  selectValue} = this.props
+    return (
       <div className="App-input-container">
-        <input className="App-input" type="text" value={userInput} onChange={getUserInput} placeholder="Buscar por nombre"/>
-      <div>
-        <label htmlFor="gender" id="gender" >Gender:  </label>
-        <select name="gender" id="gender" onChange={getUserSelect} className="select-filter">
-          <option value="Todos">All</option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-          <option value="unknown">Unknown</option>
-        </select>
-      </div>
+        <input className="App-input" type="text" value={userInput} onChange={getUserInput} placeholder="Buscar por nombre" />
+        <div>
+          <label htmlFor="gender" id="gender">Gender:  </label>
+          <select name="gender" id="gender" onChange={getUserSelect} className="select-filter" value={selectValue}>
+            <option value="Todos">All</option>
+            <option value="Male">Male</option>
+            <option value="Female" >Female</option>
+            <option value="unknown">Unknown</option>
+          </select>
+        </div>
+        <div className="radio-container">
+        <h3>Specie:</h3>
+        <div className="radio-label">
+          <label for="specie1">
+            <input onClick={getUserRadio} checked={radioValue==='All'}
+              id="specie1"
+              type="radio"
+              value="All"
+              name="species"
+            />
+            All
+          </label>
+        </div>
+        <div className="radio-label">
+          <label for="specie2">
+            <input onClick={getUserRadio} checked={radioValue==='Human'}
+              id="specie2"
+              type="radio"
+              value="Human"
+              name="species"
+            />
+            Human
+          </label>
+        </div>
+        <div className="radio-label">
+          <label for="specie3">
+            <input onClick={getUserRadio} checked={radioValue==='Alien'}
+              id="specie3"
+              type="radio"
+              value="Alien"
+              name="species"
+            />
+            Alien
+          </label>
+        </div>
+        </div>
       </div>
     );
   }
 }
 
 Filters.propTypes = {
-  userInput:PropTypes.string.isRequired,
-  getUserInput:PropTypes.func.isRequired
+  userInput: PropTypes.string.isRequired,
+  getUserInput: PropTypes.func.isRequired
 };
 export default Filters;
